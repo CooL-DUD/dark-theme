88361eeda749b487b328067881fd15bc543b435c
CooL-DUD <gani.nurgazy.03@gmail.com>
2022-02-25 22:15:05 +0600

    1st commit

---
+++ lab4/js/scripts.js
@@ -0,0 +1,12 @@
+function changeTheme() {
+    let theme = document.getElementById("dark-mode");
+    if (theme.getAttribute("href") == "css/dark-mode.css") {
+        theme.href = "css/light-mode.css"
+    } else {
+        theme.href = "css/dark-mode.css"
+    }
+}
+
+function openModal() {
+    let modal = document.getElementById()
+}
